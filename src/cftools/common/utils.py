5b03d76415430c1d4e3fa1f59e694d74212b2fa5
CRWayman <callum.wayman@gmail.com>
2025-02-26 10:34:04 -0500

    Add documentation

--- src/cftools/common/utils.py
+++ src/cftools/common/utils.py
@@ -2,28 +2,56 @@ import json
 import pandas as pd
 
 def to_dict(response):
-    # Load the data as a dictionary 
+    """
+    Use CFAPI response to read and return dictionary
+
+    Args:
+        response (http.client.HTTPResponse): API response to be read
+
+    Returns:
+        data (dict): Dictionary from loaded API JSON response
+    """
     data = json.loads(response.read())
 
     return data
 
 def to_dataframe(response):
-        cf_dict = {}
-        cf_data = to_dict(response)
-        cf_dict['time'] = cf_data['time']
-        product_idx = 0
-        product = cf_data['schema']['product'][product_idx]
-        vL = cf_data['schema']['lev']
-        VL_SWITCH = '23'
-        
-        if vL == VL_SWITCH:
-            cf_dict.update({'product': [product]*len(cf_data['time'])})
-            cf_dict.update(cf_data['values'][product])
-        else:
-            cf_dict.update(cf_data['values'])
+    """
+    Use CFAPI response to read and return as dataframe
 
-        # Create a simple dataframe from cfapi dictionary
-        df = pd.DataFrame(data=cf_dict)
-        # Convert the time column from strings to datetimes
-        df['time'] = pd.to_datetime(df['time'])
-        return df
+    Args:
+        response (http.client.HTTPResponse): API response to be read
+
+    Returns:
+        df (pandas.core.frame.DataFrame): Dataframe created from the dictionary
+        returned from the API
+    """
+    # Create an empty dictionary, cf_dict
+    cf_dict = {}
+    # Get the API response into a new dictionary, cf_data
+    cf_data = to_dict(response)
+    # Add a time key to cf_dict from cf_data
+    cf_dict['time'] = cf_data['time']
+    # Select the product name from the schema
+    product_idx = 0 # Index required because product is provided as list in schema
+    product = cf_data['schema']['product'][product_idx]
+    # Get vertical resolution from schema
+    vL = cf_data['schema']['lev']
+    # Create a switch for vertical resolution
+    VL_SWITCH = '23'
+    
+    # Use switch to check vertical resoluion
+    if vL == VL_SWITCH:
+        # Create a column for product in the case of p23 vL
+        cf_dict.update({'product': [product]*len(cf_data['time'])})
+        cf_dict.update(cf_data['values'][product])
+    else:
+        # Add a dictionary with the product key and values to cf_dict
+        cf_dict.update(cf_data['values'])
+
+    # Create a simple dataframe from cf_dict
+    df = pd.DataFrame(data=cf_dict)
+    # Convert the time column from strings to pandas datetimes
+    df['time'] = pd.to_datetime(df['time'])
+    
+    return df
--- src/cftools/core/plotting.py
+++ src/cftools/core/plotting.py
@@ -39,25 +39,30 @@ def plume_rose(chm_data, met_data, product: str, lat: int | str, lon: int | str,
         Z = Z.reshape((-1,1))
         points = np.concat((u,v),axis=1)
 
-        grid_res = grid_res
-        show_bounds = show_bounds
-
+        # Create the gridded x and y components and the interpolation of product concentration (Z)
         grid_x, grid_y = np.mgrid[np.min(u):np.max(u):grid_res,np.min(v):np.max(v):grid_res]
         z = griddata(points, Z,(grid_x,grid_y),method='linear')
 
+        # Order the gridded components into 1D array
         grid_x1D = grid_x.ravel()
         grid_y1D = grid_y.ravel()
         z1D = z.ravel()
 
+        # Get ideal plot extent
         poslim, neglim = get_plot_bounds(grid_x, grid_y)
 
+        # Create the plot
         fig, ax = plt.subplots()
         c = ax.scatter(grid_x1D,grid_y1D,c=z1D, zorder=0)
+        # Set plot extent
         ax.set_xlim(neglim, poslim)
         ax.set_ylim(neglim, poslim)
+        # Add cross hair arrows
         for s in [[poslim-1, 0], [0,poslim-1], [neglim+1, 0], [0, neglim+1]]:
                 ax.arrow(0,0, s[0], s[1],width=0.1, head_width=0.2, fc='k',zorder=1)
+        # Add colorbar
         cbar = fig.colorbar(c, ax=ax, extend='both', label=f'{product}')
+        # Check for show_bounds and then add wind speed extents
         if show_bounds:
                 circle_rad = get_radii(poslim)
                 for r in circle_rad:
@@ -66,31 +71,48 @@ def plume_rose(chm_data, met_data, product: str, lat: int | str, lon: int | str,
                         ax.add_patch(p)
         else:
               pass
+
         return fig
 
 def vertical_profile(product, data):
+        """
+        Return a plot of vertical pollutant concentration across a select time window.
+
+            Parameters:
+                    product (str): Chemical species available in CFAPI
+                    data (dict): An API response generated in cftools.core.api_call
+
+            Returns:
+                    fig (matplotlib.figure.Figure): Requested plot
+        """
+        # Get the install path and open the plot configuration file as a dictionary
         cftools_path = get_cftools_path()
         with open((cftools_path + '/config/config_plots.yml'), 'r') as ymlfile:
                 config_plot = yaml.safe_load(ymlfile)
 
+        # Create the chm values dictionary and time list
         p23_dict = data['values'][product]
         time_arr = data['time']
 
+        # Create a list of pressure levels
         for x in list(p23_dict.keys()):
                 p23_dict[float(x)] = p23_dict.pop(x)
 
+        # Convert time list to an array of pandas datetimes
         time_arr = np.array([pd.to_datetime(x) for x in time_arr])
 
+        # Subset the pressure levels list between 500 and 1000 hPA
         plevs = [x for x in list(p23_dict.keys()) if x >= 500. and x <= 1000.]
         plevs.sort()
 
+        # Create a set of gridded data from levels and pollutant concentrations
         X, Y = np.meshgrid(time_arr, plevs)
         Z = np.empty(shape = (len(plevs), len(time_arr)))
         for i in np.arange(len(time_arr)):
                 for j, k in enumerate(plevs):
                         Z[j, i] = p23_dict[k][i]
 
-        # Plotting specifications
+        # Try to register the new colormap from plot configurations
         color_map = eval(config_plot['colors'][product])
         if isinstance(color_map, dict):
                 color_map = mcolors.LinearSegmentedColormap('color_map', color_map)
@@ -106,9 +128,11 @@ def vertical_profile(product, data):
 
         mcmap.set_under('w')
         mcmap.set_over('k')
+        # Set bounds and normalization for colormap
         bounds = eval(config_plot['scale'][product])
         norm = eval(config_plot['contour']['norm'][product])
 
+        # Set plot parameters like plot title, labels, and line styling
         title = config_plot['title'][product]
         units = config_plot['units'][product]
         l_width = 0.8
@@ -144,12 +168,21 @@ def vertical_profile(product, data):
                 for te in tl:
                         te.set_bbox(dict(color='w', alpha=0.8, pad=0.1))
         except:
-                pass
                 ax.patch.set_facecolor('silver')
 
         return fig
 
 def get_plot_bounds(x, y):
+    """
+    Get the plot extent based on max U and V wind speeds.
+
+    Args:
+        x (numpy.ndarray): 1D raveled numpy array of the gridded x values
+        y (numpy.ndarray): 1D raveled numpy array of the gridded y values
+
+    Returns:
+        pos, neg (float): Two float values for the positive and negative plot limits
+    """
     lim = max(abs(x).max(), abs(y).max())
     lim = lim + 1
     pos = lim
@@ -157,9 +190,16 @@ def get_plot_bounds(x, y):
     return pos, neg
 
 def get_radii(lim):
+    """
+    Create a list of circle radii to be imposed on the plot.
+
+    Args:
+        lim (float): The plot bounds limit.
+    """
     if lim > 10:
         step = 5
     else:
         step = 1
     radii = np.arange(0,lim,step).tolist()[1:-1]
+
     return radii
